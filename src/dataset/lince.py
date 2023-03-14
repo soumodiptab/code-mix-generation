7777b972deac7a1d7d95f08e2fac47f566a64d84
Soumodipta Bose <sbose2019@gmail.com>
2023-03-14 19:06:27 +0530

    c

--- src/dataset/lince.py
+++ src/dataset/lince.py
@@ -3,8 +3,40 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class LinceDataset(Dataset):
-    def __init__(self, data):
-        x, y = self.__create_dataset(data)
+    def __init__(self, filename):
+        data = self.read_data(filename)
+        self.vocab, self.ind2vocab = self.build_vocab(data)
+        x = self.__create_dataset(data)
+
+    def __init__(self,filename,vocab):
+        self.vocab = vocab
+        self.ind2vocab = {v:k for k,v in vocab.items()}
+        data = self.read_data(filename)
+        x = self.__create_dataset(data)
+        
+    def get_vocab(self):
+        return self.vocab
+
+    def read_data(filename):
+        lines = []
+        with open(filename, 'r') as f:
+            for line in f.readlines():
+                lines.append(line.strip().split(' '))
+        return lines
+
+    def build_vocab(self,data):
+        word_set = set()
+        for line in data:
+            for word in line:
+                if word not in word_set:
+                    word_set.add(word)
+        # sort the vocab
+        word_list = sorted(list(word_set))
+        vocab_dict = {}
+        for i,word in enumerate(word_list):
+            vocab_dict[word]=i+2
+        ind2word = {v:k for k,v in vocab_dict.items()}
+        return vocab_dict, ind2word
 
     def __create_dataset(self, data):
         x = []
